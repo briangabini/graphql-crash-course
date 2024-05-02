870cd228094522a9506d54d4e948891ba2ab34ba
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-05-02 13:45:37 +0800

    feat: add query variables

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ const resolvers = {
         games() {
             return db.games;
         },
+        game(_, args, context) {
+            return db.games.find((game) => game.id === args.id) 
+        },
         authors() {
             return db.authors;
         },
+        author(_, args, context) {
+            
+            return db.authors.find((author) => author.id === args.id) 
+        },
         reviews() {
             return db.reviews;
+        },
+        review(_, args, context) {
+            
+            return db.reviews.find((review) => review.id === args.id) 
         }
     }
 }
